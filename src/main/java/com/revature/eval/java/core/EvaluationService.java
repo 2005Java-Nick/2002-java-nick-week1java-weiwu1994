d4406b2020db4a8d91bb074990938e559e41fade
Wei Wu <>
2020-05-06 21:26:28 -0400

    Assignment Completed

--- src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -137,48 +137,28 @@ public class EvaluationService {
 	 */
 	public int getScrabbleScore(String string) {
 		int score = 0;
-		for(int i = 0; i < string.length();i++) {
-			switch(string.charAt(i)) {
+		String aString = string.toLowerCase();
+		for(int i = 0; i < aString.length();i++) {
+			switch(aString.charAt(i)) {
 			case 'a': case 'e': case 'i': case 'o': case 'u': case 'l': case 'n': case 'r': case 's': case 't':
-				score+=1;
-				break;
-			case 'A': case 'E': case 'I': case 'O': case 'U': case 'L': case 'N': case 'R': case 'S': case 'T':
 				score+=1;
 				break;
 			case 'g':
 				score+=2;
 				break;
-			case 'G':
-				score+=2;
-				break;
 			case 'b': case 'c': case 'm': case 'p':
-				score+=3;
-				break;
-			case 'B': case 'C': case 'M': case 'P':
 				score+=3;
 				break;
 			case 'f': case 'h': case 'v': case 'w': case 'y':
 				score+=4;
 				break;
-			case 'F': case 'H': case 'V': case 'W': case 'Y':
-				score+=4;
-				break;
 			case 'k':
-				score+=5;
-				break;
-			case 'K':
 				score+=5;
 				break;
 			case 'j': case 'x':
 				score+=8;
 				break;
-			case 'J': case 'X':
-				score+=8;
-				break;
 			case 'q': case 'z':
-				score+=10;
-				break;
-			case 'Q': case 'Z':
 				score+=10;
 				break;
 			default:
